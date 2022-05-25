7f38bf623d20a9b04b584e8f4b8dced5a17aca59
luan <luanconsutorakushon@gmail.com>
2022-05-25 13:50:21 +0900

    .

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,14 +1,15 @@
 // import { useRouter } from "next/router";
 // import projects from "../../data/projects";
 
-// export default function Project() {
-//   const router = useRouter()
-//   const { id } = router.query
-//   const project = projects.find(project => project.name === id)
-//   return (
-//     <div className="flex flex-col items-center justify-center">
-//       <h1>Project name: {project.name}</h1>
-//       <p>Tools used: {project.tools}</p>
-//     </div>
-//   )
-// }
+export default function Project() {
+  // const router = useRouter()
+  // const { id } = router.query
+  // const project = projects.find(project => project.name === id)
+  return (
+    <div></div>
+    // <div className="flex flex-col items-center justify-center">
+    //   <h1>Project name: {project.name}</h1>
+    //   <p>Tools used: {project.tools}</p>
+    // </div>
+  )
+}
